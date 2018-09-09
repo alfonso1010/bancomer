39a20c303bea378137736270fc448b5d7e55c6f2
aarellanes <aarellanes@artificesweb.com>
2018-09-08 19:41:27 -0500

    Se agrega servicio cuenta Negocio

---
+++ application/controllers/api/CuentaNegocio.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require 'Api.php';
+
+class CuentaNegocio extends Api {
+
+  public $table_name = "Cliente";
+  public $coulumna_id = "idCliente";
+
+  public function __construct() {
+      parent::__construct();
+  }
+
+  public function index_get($id = 0){
+  	$registros =  $this->generico->obtenerRegistros([
+                      'select' => '*',
+                      'from' => "Negocio ",
+                      'where' => "where idNegocio = ".$id
+                    ]);
+  	//debug($cuenta);
+  	$datos = [];
+  	if(is_array($registros)){
+  		foreach ($registros as $key => $value) {
+  			$datos[] = [
+  				'ot' => "0001",
+  				'dOp' => [
+  					'alias' => "",
+  					'cl' => $value[0]['num_cuenta'],
+  					'type' => isset($cuenta[0]["desc_cuenta"])?$cuenta[0]["desc_cuenta"]:"TD",
+  					'refn' => "",
+  					'refa' => $value['Nombre'],
+  					'amount' => "",
+  					'bank' => "00012",
+  					'country' => "MX",
+  					'currency' => "MXN"
+  				]
+  			];
+  		}
+  	}
+  	$this->response($datos, 200);
+  }
+
+   
+}
